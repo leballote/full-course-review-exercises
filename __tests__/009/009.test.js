2b6053018874d8298011b56e99d0b82dda622a21
Luis Ballote <ealjkl@gmail.com>
2022-12-30 11:12:13 -0600

    tested 9

---
+++ __tests__/009/009.test.js
@@ -0,0 +1,42 @@
+import { flattenArray } from "../../src/009";
+
+describe("edge cases", () => {
+  test("primitive e.g. number", () => {
+    expect(flattenArray(3)).toEqual(3);
+  });
+
+  test("primitive e.g. string", () => {
+    expect(flattenArray("good bye!")).toEqual("good bye!");
+  });
+
+  test("null", () => {
+    expect(flattenArray(null)).toEqual(null);
+  });
+
+  test("0 length array", () => {
+    expect(flattenArray([])).toEqual([]);
+  });
+
+  test("1 length array", () => {
+    expect(flattenArray([1000])).toEqual([1000]);
+  });
+});
+
+describe("generic cases", () => {
+  test("example 1", () => {
+    const input = [1, 2, 3, [4, 5, [6, [[7]], 8]], [9, 10]];
+    expect(flattenArray(input)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  test("example 2", () => {
+    const input = [[[[[[[[0]]]]]]]];
+    expect(flattenArray(input)).toEqual([0]);
+  });
+
+  test("example 3", () => {
+    const input = [30, [20], 10, [2], [4], 3, [3, [5]], [7, [8, [9], [10]]]];
+    expect(flattenArray(input)).toEqual([
+      30, 20, 10, 2, 4, 3, 3, 5, 7, 8, 9, 10,
+    ]);
+  });
+});
---
+++ __tests__/009/009_iterative.test.js
@@ -0,0 +1,44 @@
+import { iterativeFlattenArray } from "../../src/009";
+
+describe("edge cases", () => {
+  test("primitive e.g. number", () => {
+    expect(iterativeFlattenArray(3)).toEqual(3);
+  });
+
+  test("primitive e.g. string", () => {
+    expect(iterativeFlattenArray("good bye!")).toEqual("good bye!");
+  });
+
+  test("null", () => {
+    expect(iterativeFlattenArray(null)).toEqual(null);
+  });
+
+  test("0 length array", () => {
+    expect(iterativeFlattenArray([])).toEqual([]);
+  });
+
+  test("1 length array", () => {
+    expect(iterativeFlattenArray([1000])).toEqual([1000]);
+  });
+});
+
+describe("generic cases", () => {
+  test("example 1", () => {
+    const input = [1, 2, 3, [4, 5, [6, [[7]], 8]], [9, 10]];
+    expect(iterativeFlattenArray(input)).toEqual([
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    ]);
+  });
+
+  test("example 2", () => {
+    const input = [[[[[[[[0]]]]]]]];
+    expect(iterativeFlattenArray(input)).toEqual([0]);
+  });
+
+  test("example 3", () => {
+    const input = [30, [20], 10, [2], [4], 3, [3, [5]], [7, [8, [9], [10]]]];
+    expect(iterativeFlattenArray(input)).toEqual([
+      30, 20, 10, 2, 4, 3, 3, 5, 7, 8, 9, 10,
+    ]);
+  });
+});
