039846757079df81e5f94f512d0a0ccff11681b1
Luis Ballote <ealjkl@gmail.com>
2023-03-08 06:28:13 -0600

    Finished tests for 44

--- src/react-exercises/__tests__/044/useIndexDB.test.tsx
+++ src/react-exercises/__tests__/044/useIndexDB.test.tsx
@@ -8,94 +8,158 @@ import {
   afterEach,
   beforeEach,
 } from "vitest";
-import setGlobalVars from "indexeddbshim/src/node-UnicodeIdentifiers";
-setGlobalVars(); // See signature below
+// import setGlobalVars from "indexeddbshim/src/node-UnicodeIdentifiers";
+// setGlobalVars(); // See signature below
+import "fake-indexeddb/auto";
 import { render, waitFor } from "@testing-library/react";
 import useIndexDB from "../../src/044/hooks/useIndexDB";
 import { renderHook, act } from "@testing-library/react";
 import * as idb from "idb";
 
-beforeEach(() => {
-  localStorage.clear();
-});
-
 describe("State is sychronized with useIndexDB", () => {
   test("At initialization", async () => {
-    const { result } = renderHook(() => useIndexDB("foo", 5));
-    expect(result.current[0]).toEqual(5);
-    const mydb = await idb.openDB("useIndexDB");
-    await waitFor(async () => {
-      expect(result.current[2].current == null).toBe(false);
+    const mydb = await idb.openDB("useIndexDB", 1, {
+      upgrade(db) {
+        db.createObjectStore("state");
+      },
     });
-
-    await waitFor(async () => {
-      const data = await mydb.getAll("state", "foo");
-      expect(data == undefined).toBe(false);
+    const { result } = renderHook(() => useIndexDB("foo", 6));
+    expect(result.current[0]).toEqual(6);
+    // const mydb = await idb.openDB("useIndexDB");
+    await waitFor(() => {
+      expect(result.current[2]).toBe(true);
     });
-    const data = await mydb.get("state", "foo");
-    console.log("data outside", data);
-    // console.log(result.current[2].current);
+    const valueFromDB = await mydb.get("state", "foo");
+    expect(valueFromDB).toEqual(6);
   });
 
-  // test("After changing state", () => {
-  //   const { result } = renderHook(() => useIndexDB("foo", 5));
-  //   act(() => {
-  //     result.current[1](11);
-  //   });
-  //   expect(result.current[0]).toEqual(11);
-  // });
+  test("After changing state", async () => {
+    const mydb = await idb.openDB("useIndexDB2", 1, {
+      upgrade(db) {
+        db.createObjectStore("state");
+      },
+    });
+    const { result } = renderHook(() =>
+      useIndexDB("foo", 6, {
+        dbName: "useIndexDB2",
+      })
+    );
+
+    act(() => {
+      result.current[1](11);
+    });
+    await waitFor(async () => {
+      const valueFromDB = await mydb.get("state", "foo");
+      expect(valueFromDB).toEqual(11);
+    });
+    expect(result.current[0]).toEqual(11);
+  });
 });
 
-// describe("It should accept objects as initial state", () => {
-//   test("simple object", () => {
-//     const { result } = renderHook(() => useIndexDB("foo", { baz: true }));
-//     expect(result.current[0]).toEqual({ baz: true });
-//     const valueFromStorage1 = localStorage.getItem("foo");
-//     expect(valueFromStorage1).not.toEqual(null);
-//     expect(result.current[0]).toEqual(JSON.parse(valueFromStorage1!));
-//     act(() => {
-//       result.current[1]({ baz: false });
-//     });
-//     expect(result.current[0]).toEqual({ baz: false });
-//     const valueFromStorage2 = localStorage.getItem("foo");
-//     expect(valueFromStorage2).not.toEqual(null);
-//     expect(result.current[0]).toEqual(JSON.parse(valueFromStorage2!));
-//   });
+describe("It should accept objects as initial state", () => {
+  test("simple object", async () => {
+    const mydb = await idb.openDB("useIndexDB3", 1, {
+      upgrade(db) {
+        db.createObjectStore("state");
+      },
+    });
+    const { result } = renderHook(() =>
+      useIndexDB("foo", { baz: true }, { dbName: "useIndexDB3" })
+    );
+    expect(result.current[0]).toEqual({ baz: true });
+    await waitFor(async () => {
+      const valueFromDB = await mydb.get("state", "foo");
+      expect(valueFromDB).toEqual({ baz: true });
+    });
+    act(() => {
+      result.current[1]({ baz: false });
+    });
+    expect(result.current[0]).toEqual({ baz: false });
 
-//   test("complex objects", () => {
-//     const { result } = renderHook(() =>
-//       useLocalStorage<object>("foo", {
-//         foo: {
-//           a: [1, 5, { hey: 7 }],
-//         },
-//       })
-//     );
-//     expect(result.current[0]).toEqual({
-//       foo: {
-//         a: [1, 5, { hey: 7 }],
-//       },
-//     });
-//     const valueFromStorage1 = localStorage.getItem("foo");
-//     expect(valueFromStorage1).not.toEqual(null);
-//     expect(result.current[0]).toEqual(JSON.parse(valueFromStorage1!));
-//     act(() => {
-//       result.current[1]({
-//         foo: {
-//           a: [1],
-//           b: [7, true, "haha"],
-//           c: 4,
-//         },
-//       });
-//     });
-//     expect(result.current[0]).toEqual({
-//       foo: {
-//         a: [1],
-//         b: [7, true, "haha"],
-//         c: 4,
-//       },
-//     });
-//     const valueFromStorage2 = localStorage.getItem("foo");
-//     expect(valueFromStorage2).not.toEqual(null);
-//     expect(result.current[0]).toEqual(JSON.parse(valueFromStorage2!));
-//   });
-// });
+    await waitFor(async () => {
+      const valueFromDB = await mydb.get("state", "foo");
+      expect(valueFromDB).toEqual({ baz: false });
+    });
+  });
+
+  test("complex objects", async () => {
+    const mydb = await idb.openDB("useIndexDB4", 1, {
+      upgrade(db) {
+        db.createObjectStore("state");
+      },
+    });
+    const { result } = renderHook(() =>
+      useIndexDB(
+        "foo",
+        {
+          foo: {
+            a: [1, 5, { hey: 7 }],
+          },
+        },
+        { dbName: "useIndexDB4" }
+      )
+    );
+    expect(result.current[0]).toEqual({
+      foo: {
+        a: [1, 5, { hey: 7 }],
+      },
+    });
+    await waitFor(async () => {
+      const valueFromDB = await mydb.get("state", "foo");
+      expect(valueFromDB).toEqual({
+        foo: {
+          a: [1, 5, { hey: 7 }],
+        },
+      });
+    });
+    act(() => {
+      result.current[1]({
+        foo: {
+          a: [1],
+          b: [7, true, "haha"],
+          c: 4,
+        },
+      } as any);
+    });
+    expect(result.current[0]).toEqual({
+      foo: {
+        a: [1],
+        b: [7, true, "haha"],
+        c: 4,
+      },
+    });
+
+    await waitFor(async () => {
+      const valueFromDB = await mydb.get("state", "foo");
+      expect(valueFromDB).toEqual({
+        foo: {
+          a: [1],
+          b: [7, true, "haha"],
+          c: 4,
+        },
+      });
+    });
+  });
+});
+
+test("It should fetch the data from the database as initial value", async () => {
+  const mydb = await idb.openDB("useIndexDB5", 1, {
+    upgrade(db) {
+      db.createObjectStore("state");
+    },
+  });
+  renderHook(() => useIndexDB("foo", "hi", { dbName: "useIndexDB5" }));
+
+  await waitFor(async () => {
+    const valueFromDB = await mydb.get("state", "foo");
+    expect(valueFromDB).toEqual("hi");
+  });
+
+  const { result: result2 } = renderHook(() =>
+    useIndexDB("foo", "bye", { dbName: "useIndexDB5" })
+  );
+
+  await waitFor(async () => {
+    expect(result2.current[0]).toEqual("hi");
+  });
+});
--- src/react-exercises/src/044/hooks/useIndexDB.ts
+++ src/react-exercises/src/044/hooks/useIndexDB.ts
@@ -15,40 +15,46 @@ export default function useIndexDB<T>(
     storeName = "state",
     placeHolderValue,
   }: {
-    dbName: string;
-    storeName: string;
+    dbName?: string;
+    storeName?: string;
     placeHolderValue?: T;
   } = {} as any
 ) {
   const [val, setVal] = useState<T>(placeHolderValue ?? initialValue);
   const [db, setDB] = useState<idb.IDBPDatabase | null>(null);
   const [initiated, setInitiated] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
 
   async function init() {
     //if db is already set it shoudln't do anything
     if (db) return;
-    console.log("initing");
-    const _db = await idb.openDB(dbName, 1, {
-      upgrade(db) {
-        if (!db.objectStoreNames.contains(storeName)) {
-          db.createObjectStore(storeName);
-        }
-        //here it should not sync values since after setting db, it will p
-      },
-    });
-    setDB(_db);
-
     try {
+      const _db = await idb.openDB(dbName, 1, {
+        upgrade(db) {
+          if (!db.objectStoreNames.contains(storeName)) {
+            db.createObjectStore(storeName);
+          }
+          //here it should not sync values since after setting db, it will p
+        },
+      });
+
       if ((await _db.get(storeName, key)) == undefined) {
         await _db.put(storeName, initialValue, key);
+
+        setDB(() => _db);
+        setInitiated(() => true);
         return initialValue;
       }
       const valueFromDB = await _db.get(storeName, key);
       if (valueFromDB === undefined) return initialValue;
-      setVal(valueFromDB);
-      setInitiated(true);
+      //all the sets must be together so the updates get batched
+      setDB(() => _db);
+      //initiated is a bit redundant because the db not null means is initiated, but anyways this seems more clear
+      setInitiated(() => true);
+      setVal(() => valueFromDB);
     } catch (e) {
-      console.log("here I should return");
+      //if for some reason we couldn't open the db then we can't persist the data nor fetch the data from the indexDB
+      setError(e as Error);
       return initialValue;
     }
   }
@@ -64,15 +70,13 @@ export default function useIndexDB<T>(
   }, []);
 
   useEffect(() => {
-    console.log(val, db, initiated);
-    if (initiated) {
-      syncValues(val, db);
-    }
-  }, [val, db, initiated]);
+    syncValues(val, db);
+  }, [val, db]);
 
-  return [val, setVal, initiated] as [
+  return [val, setVal, initiated, error] as [
     typeof val,
     typeof setVal,
-    typeof initiated
+    typeof initiated,
+    typeof error
   ];
 }
